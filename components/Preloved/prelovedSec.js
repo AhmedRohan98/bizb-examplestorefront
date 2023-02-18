6f36b63726cc060022483412d60c0efe87c0034c
sardar umer <umer.javed@codistan.org>
2023-02-18 23:46:01 +0500

    preloved section completed

--- components/Preloved/prelovedSec.js
+++ components/Preloved/prelovedSec.js
@@ -1,333 +1,201 @@
-// import React from "react";
-// import Container from "@material-ui/core/Container";
-// import Typography from "@material-ui/core/Typography";
-// import Grid from "@material-ui/core/Grid";
-
-// import ImageList from "@material-ui/core/ImageList";
-// import ImageListItem from "@material-ui/core/ImageListItem";
-// import ImageListItemBar from "@material-ui/core/ImageListItemBar";
-// import Box from "@material-ui/core/Box";
-// import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
-// import { makeStyles } from "@material-ui/core/styles";
-
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     display: "flex",
-//     flexWrap: "wrap",
-//     justifyContent: "space-around",
-//     overflow: "hidden",
-//     backgroundColor: theme.palette.background.paper,
-//   },
-//   root2: {
-//     display: "flex",
-//     justifyContent: "center",
-//     alignItems: "center",
-//     marginBottom: theme.spacing(6),
-//   },
-//   PrelovedHeader: {
-//     display: "flex",
-//     justifyContent: "center",
-
-//     flexDirection: "column",
-//     alignItems: "center",
-    
-//   },
-//   PrelovedHeader2: {
-//     display: "flex",
-//     justifyContent: "center",
-//     flexDirection: "row",
-//     alignItems: "center",
-//   },
-
-//   titleBar: {
-//     background: "linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, " + "rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)",
-//   },
-//   icon: {
-//     color: "white",
-//   },
-//   subtitle: {
-//     width: "543px",
-//     height: "87px",
-//     display: "flex",
-//     align: "center",
-//     justifyContent: "center",
-//     alignItems: "center",
-//   },
-//   subtitlet: {
-//     color: "#1F1F1F",
-
-//     fontFamily: "Lato",
-//     textAlign: "center",
-//   },
-//   carts: {
-//     position: "relative",
-//   },
-//   cart: {
-//     height: "35px",
-//     width: "84px",
-   
-//     bottom: "50%",
-//     left: "10px",
-//     position: "absolute",
-//   },
-//   cart2: {
-//     height: "35px",
-//     width: "84px",
-  
-   
-//     bottom: "50%",
-//     right: "70px",
-//     position: "absolute",
-//   },
-//   imagec:{
-//     width:"100%"
-//   },
-//   mobileicon:{
-//     display:"flex",
-//     alignItems:"center",
-//     justifyContent:"center",
-//     paddingBottom:theme.spacing(3),
-//     [theme.breakpoints.up(900)]: {
-//    display:"none"
-//     }
-    
-//   },
-//   text:{
-//     color :theme.palette.primary.contrastText,
-//     textTransform: "uppercase",
-//   },
-//   buttonshop:{
-//     background:theme.palette.secondary.selected,
-//     display:"flex",
-//     justifyContent:"center",
-//     alignItems:"center",
-//     width:"150px",
-//     height:"35px",
-//    right:"50px",
-//     borderRadius:"40px",
-  
- 
-//   },
-//   buttonshopt:{
- 
-//     fontSize: "22px",
-//     color:"#000000",
-//   fontWeight:900,
-//     fontStyle: "Black",
-//     lineHeight:"26px",
-
-//       fontFamily: "Ostrich Sans",
-//   },
-//   mobileview:{
-//     height:"60px",
-//     width:"350px",
-//     display:"flex",
-//     flexDirection:"row",
-//     justifyContent:"space-evenly",
-//     paddig:"8px",
-//     alignItems:"center",
-//     background:theme.palette.reaction.black80,
-//     borderRadius:"90px",
-//     zIndex: 9999,
-//    bottom:"10px",
-//     position:"fixed",
-//     display:"none",
-//     [theme.breakpoints.down(700)]: {
-//       display:"block",
-//       height:"60px",
-//       width:"350px",
-//       display:"flex",
-//       flexDirection:"row",
-//       justifyContent:"space-evenly",
-//       paddig:"8px",
-//       alignItems:"center",
-//       background:theme.palette.reaction.black80,
-//       borderRadius:"90px",
-//       zIndex: 9999,
-//      bottom:"10px",
-//       position:"fixed",
-         
-//              },
-//   },
-//   mobileviewfixed:{
-//     display:"flex",
-//     flexDirection: "row",
-//     alignItems: "space-evenly",
-//     justifyContent: "sapce-between",
-//   },
-//   mobileviewfixedText:{
-//     color:"#ffffff"
-//   },
-//   imagess:{
-//     width:"100%",
-//     height:"100%",
-//     objectFit: "cover",
-//   }
-// }));
-// const itemData = [
-//   {
-//     image: "/preloved/preloved1.svg",
-//     id: 1,
-//     title: "Causal",
-//     name:"CAUSAL"
-//   },
-
-//   {
-//     image: "/preloved/preloved2.svg",
-//     title: "Causal",
-//     id: 2,
-//     name:"WESTREN"
-//   },
-
-
-//   {
-//     image: "/preloved/preloved3.svg",
-//     id: 3,
-//     title: "Juniors",
-//     name:"PARTY"
-//   },
-//   {
-//     image: "/preloved/preloved5.svg",
-//     id: 5,
-//     title: "Causal",
-//     name:"SHOES",
-//   },
-//   {
-//     image: "/preloved/preloved6.svg",
-//     id: 6,
-//     title: "Causal",
-//     name:"Assesories"
-//   },
-//   {
-//     image: "/preloved/preloved4.svg",
-//     id: 4,
-//     title: "Juniors",
-//     name:"Bridal"
-//   },
-
- 
-//   {
-//     image: "/preloved/preloved7.svg",
-//     id: 7,
-//     title: "Causal",
- 
-//     name:"JUNIORS"
-//   },
-
-// ];
-
-// const Preloved = () => {
-//   const classes = useStyles();
-//   return (
-//     <>
-//       <Container className={classes.PrelovedHeader}>
-//         <Typography variant="h1">Share Your</Typography>
-//         <Container className={classes.PrelovedHeader2}>
-//           <Typography variant="h2">Pre-Loved</Typography>
-//           <Typography variant="h1">Fashion</Typography>
-//         </Container>
-//       </Container>
-//       <div className={classes.root2}>
-//         <Box className={classes.subtitle}>
-//           <Typography variant="h4" className={classes.subtitlet}>
-//             Now you can revamp your daily wear wardrobe every month while saving more than 50% from your monthly budget!
-//           </Typography>
-//         </Box>
-//         <div className={classes.mobileview}>
-//           <div className={classes.mobileviewfixed}>
-//             <img src="/app-section/home.svg" />
-//             <Typography variant="h5" className={classes.mobileviewfixedText}> Home </Typography>
-//           </div>
-//           <div className={classes.mobileviewfixed} >     <img src="/app-section/search.svg"  />
-//           <Typography variant="h5" className={classes.mobileviewfixedText}> Sell</Typography>
-//           </div>
-//           <div className={classes.mobileviewfixed}>    <img src="/app-section/explore.svg"  />
-//           <Typography variant="h5" className={classes.mobileviewfixedText}> Explore</Typography>
-//           </div>
-//         </div>
-//       </div>
-     
-//       <Container className={classes.mobileicon}  display={{ lg: 'block', xl: 'none' ,sm:"none" }}>
-//       <img src="/preloved/preloved-mobile.svg"  />
-//         </Container>
-//         <Grid container >
-       
-//         <Grid item xs={12} md={6} sm={6} lg={6} >
-//         <img src="/preloved/preloved1.svg"  className={classes.imagess} />
-//         </Grid>
-//         <Grid item xs={12} md={6} sm={6} lg={6}>
-//         <img src="/preloved/preloved2.svg"  className={classes.imagess}/>
-//         </Grid>
-//         <Grid container item xs={8}>
-//         <Grid item xs={12} md={6} sm={3} lg={3}>
-//         <img src="/preloved/preloved3.svg"  className={classes.imagess}/>
-//         </Grid>
-//         <Grid item xs={12} md={6} sm={5} lg={5}>
-//         <img src="/preloved/preloved4.svg" className={classes.imagess} />
-//         </Grid>
-//         </Grid>
-//         <Grid container item xs={4}>
-
-// <Grid item xs={12} md={6} sm={12} lg={12}>
-//   <img src="/preloved/preloved6.png" className={classes.imagess} />
-//   </Grid>
-
-// </Grid>
-       
-        
-//       </Grid>
-//       <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 1200: 3 }}>
-//         <Masonry>
-//           {itemData.map((item) => (
-//             <>
-//               <div className={classes.carts}>
-//                 <img src={item.image} className={classes.imagec} />
-
-//                 <div className={item.title === "Causal" ? classes.cart : classes.cart2}>
-            
-//                   <Typography gutterBottom variant="h3"  className={classes.text}>
-//                    {item.name}
-//                   </Typography>
-//                   <div className={classes.buttonshop}>  <h4  className={classes.buttonshopt}>SHOP NOW</h4></div>
-                 
-//                 </div>
-//               </div>
-//             </>
-//           ))}
-//         </Masonry>
-//       </ResponsiveMasonry>
-//     </>
-//   );
-// };
-
-// export default Preloved;
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
-import { Card, CardContent, List, ListItem } from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    overflow: "hidden",
+    backgroundColor: theme.palette.background.paper,
+  },
+  root2: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: theme.spacing(6),
+  },
+  PrelovedHeader: {
+    display: "flex",
+    justifyContent: "center",
 
+    flexDirection: "column",
+    alignItems: "center",
+    
+  },
+  PrelovedHeader2: {
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+
+  titleBar: {
+    background: "linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, " + "rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)",
+  },
+  icon: {
+    color: "white",
+  },
+  subtitle: {
+    width: "543px",
+    height: "87px",
+    display: "flex",
+    align: "center",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  subtitlet: {
+    color: "#1F1F1F",
+
+    fontFamily: "Lato",
+    textAlign: "center",
+  },
+  carts: {
+    position: "relative",
+  },
+  cart: {
+    height: "35px",
+    width: "84px",
+   
+    bottom: "50%",
+    left: "10px",
+    position: "absolute",
+  },
+  cart2: {
+    height: "35px",
+    width: "84px",
+  
+   
+    bottom: "50%",
+    right: "70px",
+    position: "absolute",
+  },
+  imagec:{
+    width:"100%"
+  },
+  mobileicon:{
+    display:"flex",
+    alignItems:"center",
+    justifyContent:"center",
+    paddingBottom:theme.spacing(3),
+    [theme.breakpoints.up(900)]: {
+   display:"none"
+    }
+    
+  },
+  text:{
+    color :theme.palette.primary.contrastText,
+    textTransform: "uppercase",
+  },
+  buttonshop:{
+    background:theme.palette.secondary.selected,
+    display:"flex",
+    justifyContent:"center",
+    alignItems:"center",
+    width:"150px",
+    height:"35px",
+   right:"50px",
+    borderRadius:"40px",
+  
+ 
+  },
+  buttonshopt:{
+ 
+    fontSize: "22px",
+    color:"#000000",
+  fontWeight:900,
+    fontStyle: "Black",
+    lineHeight:"26px",
+
+      fontFamily: "Ostrich Sans",
+  },
+  mobileview:{
+    height:"60px",
+    width:"350px",
+    display:"flex",
+    flexDirection:"row",
+    justifyContent:"space-evenly",
+    paddig:"8px",
+    alignItems:"center",
+    background:theme.palette.reaction.black80,
+    borderRadius:"90px",
+    zIndex: 9999,
+   bottom:"10px",
+    position:"fixed",
+    display:"none",
+    [theme.breakpoints.down(700)]: {
+      display:"block",
+      height:"60px",
+      width:"350px",
+      display:"flex",
+      flexDirection:"row",
+      justifyContent:"space-evenly",
+      paddig:"8px",
+      alignItems:"center",
+      background:theme.palette.reaction.black80,
+      borderRadius:"90px",
+      zIndex: 9999,
+     bottom:"10px",
+      position:"fixed",
+         
+             },
+  },
+  mobileviewfixed:{
+    display:"flex",
+    flexDirection: "row",
+    alignItems: "space-evenly",
+    justifyContent: "sapce-between",
+  },
+  mobileviewfixedText:{
+    color:"#ffffff"
+  },
   imagess:{
-        width:"100%",
-        height:"100%",
-        objectFit: "cover",
-      }
+    width:"100%",
+    height:"100%",
+    objectFit: "cover",
+  }
 }));
 
-export default function Preloved() {
+const Preloved = () => {
   const classes = useStyles();
 
   function FormRow1() {
     return (
       <React.Fragment>
-        <Grid item xs={12} md={6} sm={6} lg={6}>
-          <img src="/preloved/preloved1.svg"  className={classes.imagess} />
+        <Grid item xs={12} md={6} sm={12} lg={6} className={classes.carts}>
+     
+        <img src="/preloved/preloved1.svg"  className={classes.imagess} />
+
+                <div className={classes.cart}>
+            
+                  <Typography gutterBottom variant="h3"  className={classes.text}>
+                  CAUSAL
+                  </Typography>
+                  <div className={classes.buttonshop}>  <h4  className={classes.buttonshopt}>SHOP NOW</h4></div>
+                 
+                </div>
+            
+        
         </Grid>
-        <Grid item xs={12} md={6} sm={6} lg={6}>
-          <img src="/preloved/preloved2.svg"  className={classes.imagess} />
+        <Grid item xs={12} md={6} sm={12} lg={6}className={classes.carts}>
+      
+        <img src="/preloved/preloved2.svg"  className={classes.imagess} />
+
+                <div className={classes.cart}>
+            
+                  <Typography gutterBottom variant="h3"  className={classes.text}>
+                  Western
+                  </Typography>
+                  <div className={classes.buttonshop}>  <h4  className={classes.buttonshopt}>SHOP NOW</h4></div>
+                 
+                </div>
+              
+        
         </Grid>
       </React.Fragment>
     );
@@ -335,11 +203,35 @@ export default function Preloved() {
   function FormRow4() {
     return (
       <React.Fragment>
-        <Grid item xs={5}>
-          <img src="/preloved/preloved3.svg"  className={classes.imagess} />
+        <Grid item  xs={12} md={5} sm={12} lg={5} className={classes.carts}>
+
+        <img src="/preloved/preloved3.svg"  className={classes.imagess} />
+
+        <div className={classes.cart2}>
+            
+            <Typography gutterBottom variant="h3"  className={classes.text}>
+            Party 
+            </Typography>
+            <div className={classes.buttonshop}>  <h4  className={classes.buttonshopt}>SHOP NOW</h4></div>
+           
+          </div>
+    
+          
+   
         </Grid>
-        <Grid item xs={7}>
-          <img src="/preloved/preloved4.svg"  className={classes.imagess} />
+        <Grid item xs={12} md={7} sm={12} lg={7} className={classes.carts}>
+
+        <img src="/preloved/preloved4.svg"  className={classes.imagess} />
+
+<div className={classes.cart2}>
+    
+    <Typography gutterBottom variant="h3"  className={classes.text}>
+    Bridal
+    </Typography>
+    <div className={classes.buttonshop}>  <h4  className={classes.buttonshopt}>SHOP NOW</h4></div>
+   
+  </div>
+         
         </Grid>
       </React.Fragment>
     );
@@ -347,11 +239,37 @@ export default function Preloved() {
   function FormRow3() {
     return (
       <React.Fragment>
-        <Grid item xs={6}>
+        <Grid item xs={12} md={6} sm={12} lg={6}  className={classes.carts}>
+
         <img src="/preloved/preloved5.svg"  className={classes.imagess} />
+
+<div className={classes.cart}>
+
+<Typography gutterBottom variant="h3"  className={classes.text}>
+Shoes
+</Typography>
+<div className={classes.buttonshop}>  <h4  className={classes.buttonshopt}>SHOP NOW</h4></div>
+
+</div>
+ 
+
+ 
         </Grid>
-        <Grid item xs={6}>
-        <img src="/preloved/preloved7.svg"  className={classes.imagess} />
+        <Grid item xs={12} md={6} sm={12} lg={6}
+        className={classes.carts}>
+
+<img src="/preloved/preloved7.svg"  className={classes.imagess} />
+
+<div className={classes.cart}>
+
+<Typography gutterBottom variant="h3"  className={classes.text}>
+Juniors
+</Typography>
+<div className={classes.buttonshop}>  <h4  className={classes.buttonshopt}>SHOP NOW</h4></div>
+
+</div>
+ 
+     
         </Grid>
       </React.Fragment>
     );
@@ -360,31 +278,76 @@ export default function Preloved() {
     return (
       <React.Fragment>
        
-        <Grid item xs={12} container>
-     
-          <img src="/preloved/preloved6.png"  className={classes.imagess} />
+        <Grid item xs={12} container  className={classes.carts}>
+
+        <img src="/preloved/preloved6.png"  className={classes.imagess} />
+
+<div className={classes.cart}>
+
+<Typography gutterBottom variant="h3"  className={classes.text}>
+Accessories
+</Typography>
+<div className={classes.buttonshop}>  <h4  className={classes.buttonshopt}>SHOP NOW</h4></div>
+
+</div>
+       
         </Grid>
       </React.Fragment>
     );
   }
 
-
   return (
-    <div>
+    <>
+      <Container className={classes.PrelovedHeader}>
+        <Typography variant="h1">Share Your</Typography>
+        <Container className={classes.PrelovedHeader2}>
+          <Typography variant="h2">Pre-Loved</Typography>
+          <Typography variant="h1">Fashion</Typography>
+        </Container>
+      </Container>
+      <div className={classes.root2}>
+        <Box className={classes.subtitle}>
+          <Typography variant="h4" className={classes.subtitlet}>
+            Now you can revamp your daily wear wardrobe every month while saving more than 50% from your monthly budget!
+          </Typography>
+        </Box>
+        <div className={classes.mobileview}>
+          <div className={classes.mobileviewfixed}>
+            <img src="/app-section/home.svg" />
+            <Typography variant="h5" className={classes.mobileviewfixedText}> Home </Typography>
+          </div>
+          <div className={classes.mobileviewfixed} >     <img src="/app-section/search.svg"  />
+          <Typography variant="h5" className={classes.mobileviewfixedText}> Sell</Typography>
+          </div>
+          <div className={classes.mobileviewfixed}>    <img src="/app-section/explore.svg"  />
+          <Typography variant="h5" className={classes.mobileviewfixedText}> Explore</Typography>
+          </div>
+        </div>
+      </div>
+     
+      <Container className={classes.mobileicon}  display={{ lg: 'block', xl: 'none' ,sm:"none" }}>
+      <img src="/preloved/preloved-mobile.svg"  />
+        </Container>
+      <div>
       <Grid container >
       <Grid container item  xs={12} md={12} sm={12} lg={12} >
           <FormRow1 />
          
         </Grid>
-        <Grid container item xs={8} >
+        <Grid container item  xs={12} md={8} sm={12} lg={8} >
           <FormRow4 />
           <FormRow3 />
         </Grid>
-        <Grid container item xs={4} >
+        <Grid container item   xs={12} md={4} sm={12} lg={4}>
           <FormRow2 />
         </Grid>
      
       </Grid>
     </div>
+    </>
   );
-}
+};
+
+export default Preloved;
+
+
