c24d34d7e12ee18da622ee9b46c42c992297f40f
sardar umer <umer.javed@codistan.org>
2023-02-22 20:31:52 +0500

    setting categories detail page layout with dummy data

--- pages/[lang]/categories/[tagId].js
+++ pages/[lang]/categories/[tagId].js
@@ -5,63 +5,189 @@ import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 const useStyles = makeStyles((theme) => ({
-mainimage:{
-  position:"relative",
-  display: 'inline-grid'
-},
-text:{
-  display:"flex",
-  justifyContent:"center",
-  alignItems:"center",
-  flexDirection: "row",
-  position:"absolute",
+  mainimage: {
+    position: "relative",
+    display: "inline-grid",
+  },
+  text: {
+    
+    position: "absolute",
+    top: "30px",
+    left: "30px",
+  },
+  texts: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+   
+  },
+  categoriesname: {
+    fontSize: "48px",
+    color: "#ffffff",
+    fontFamily: "Ostrich Sans",
+    fontWeight: 900,
 
-},
-categoriesname:{
-  top:"30px",
-  left:"30px",
-  color:"green",
-
-  fontSize: "48px",
-        color:"#ffffff",
-        fontFamily:"Ostrich Sans",
-          fontWeight: 900,
-         
-          lineHeight:"58px",
-          fontStyle: "normal",
-}
-
+    lineHeight: "58px",
+    fontStyle: "normal",
+  },
+  image:{
+    marginLeft:theme.spacing(4)
+  },
 
 }));
 function Categories(props) {
+  const ITEMS = [
+    {
+      image: '/categories/categoriestoggle.svg',
+   id:1,
+   title:"Charsssssssssserizma Store"
+    },
+    {
+      image: '/categories/sub1.svg',
+   id:2,
+   title:"Charizma Store"
+    },
+    {
+      image: '/categories/sub2.svg',
+      id:3,
+      title:"Charizma Store"
+    },
+    {
+      image: '/categories/sub3.svg',
+      id:4,
+      title:"Charizma Store"
+    },
+    {
+      image: '/categories/sub4.svg',
+      id:5,
+      title:"Charizma Store"
+    },
+    {
+      image: '/categories/sub1.svg',
+   id:6,
+   title:"Charizma Store"
+    },
+    {
+      image: '/categories/sub2.svg',
+      id:7,
+      title:"Charizma Store"
+    },
+    {
+      image: '/categories/sub3.svg',
+      id:8,
+      title:"Charizma Store"
+    },
+    {
+      image: '/categories/sub4.svg',
+      id:9,
+      title:"Charizma Store"
+    },
+    {
+      image: '/categories/sub5.svg',
+      id:10,
+      title:"Charizma Store"
+    },
+    {
+      image: '/categories/sub6.svg',
+      id:11,
+      title:"Charizma Store"
+    },
+    {
+      image: '/categories/sub1.svg',
+   id:6,
+   title:"Charizma Store"
+    },
+    {
+      image: '/categories/sub2.svg',
+      id:7,
+      title:"Charizma Store"
+    },
+    {
+      image: '/categories/sub3.svg',
+      id:8,
+      title:"Charizma Store"
+    },
+    {
+      image: '/categories/sub4.svg',
+      id:9,
+      title:"Charizma Store"
+    },
+    {
+      image: '/categories/sub5.svg',
+      id:10,
+      title:"Charizma Store"
+    },
+    {
+      image: '/categories/sub6.svg',
+      id:11,
+      title:"Charizma Store"
+    },
+  ];
+  
+const data=ITEMS.splice(0,5)
+const data2=ITEMS.splice(5,ITEMS.length)
+var firstarray = data.reduce((acc,item,index) => {
+  acc[`names${index}`] = item;
+  return acc;
+}, {});
+console.log(data2 ,"fffffffffffsardarffffffffffffffffffsadrtttt")
   const router = useRouter();
   const classes = useStyles();
   if (router.isFallback) {
     return "loading...";
   }
-  const [catalogItems, setCatalogItems] = React.useState(props.category.catalogItems.edges.map((item) => item.node?.product))
-console.log(catalogItems)
+  const [catalogItems, setCatalogItems] = React.useState(
+    props.category.catalogItems.edges.map((item) => item.node?.product),
+  );
+  console.log(catalogItems);
 
   return (
     <>
-      <Grid container lg={12} sm={12} md={12} align = "center" justify = "center" alignItems = "center">
-              <Grid  item  className={classes.grid1} lg={6} xs={6} sm={6} md={6}  >
-             <div className={classes.mainimage}>
-            
-<div className={classes.text}>
-           <Typography  variant="h1" className={classes.categoriesname}>
-              Western
-            </Typography>
-            <img src="/categories/categoriestoggle.svg"  className={classes.image}/>
+      {typeof window !== "undefined" && (
+        <Grid container lg={12} sm={12} md={12} align="center" justify="center" alignItems="center">
+          <Grid item className={classes.grid1} lg={6} xs={12} sm={6} md={12}>
+            <div className={classes.mainimage}>
+              <div className={classes.text}>
+              <div className={classes.texts}>
+              <Typography variant="h1" className={classes.categoriesname}>
+                  Western
+                </Typography>
+                <img src={firstarray.names0.image}className={classes.categorytoggle} />
+              </div>
+                <Typography variant="h1" className={classes.categoriesname}>
+                  Western
+                </Typography>
+              </div>
+              <img src="/categories/mainCategory.svg" className={classes.image} />
             </div>
-            <img src="/categories/mainCategory.svg"  className={classes.image}/>
-           </div>
-              </Grid>
-              <Grid item lg={6} sm={6} md={6} xs={6} className={classes.grid1}>
-             
-              
-              </Grid>
-            </Grid>
+          </Grid>
+          <Grid item lg={6} sm={6} md={12} xs={12}    container  
+  direction="row"
+  justifyContent="center"
+  alignItems="flex-start">
+      <img src={firstarray.names1.image} className={classes.image} />
+      <img src={firstarray.names2.image} className={classes.image} />
+      <img src={firstarray.names3.image} className={classes.image} />
+      <img src={firstarray.names4.image} className={classes.image} />
+   
+      
+          </Grid>
+          <Grid item lg={12} sm={12} md={12} xs={12}    container  
+  direction="row"
+  justifyContent="center"
+  alignItems="flex-start">
+     { data2.map((item)=>
+      <>
+   <img src ={item.image} />
+      </>
+  )
+     }
+      
+
+          </Grid>
+        </Grid>
+      )}
     </>
   );
 }
@@ -70,9 +196,9 @@ export async function getStaticPaths() {
   const tags = await fetchTags("cmVhY3Rpb24vc2hvcDp4TW1NRmFOR2I0TGhDY3dNeg==");
   let paths = tags.tags.nodes.map((tag) => ({
     params: {
-      lang:'en',
-      tagId:tag._id
-    }
+      lang: "en",
+      tagId: tag._id,
+    },
   }));
 
   return {
@@ -81,10 +207,10 @@ export async function getStaticPaths() {
   };
 }
 
-export async function getStaticProps({params: {lang, tagId}}) {
-  const categories=await fetchAllCategories(["cmVhY3Rpb24vc2hvcDp4TW1NRmFOR2I0TGhDY3dNeg=="],[tagId])
+export async function getStaticProps({ params: { lang, tagId } }) {
+  const categories = await fetchAllCategories(["cmVhY3Rpb24vc2hvcDp4TW1NRmFOR2I0TGhDY3dNeg=="], [tagId]);
 
-  console.log(categories)
+  console.log(categories);
   return {
     props: {
       category: categories,
