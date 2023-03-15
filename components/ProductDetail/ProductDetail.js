fa2bc0daa8282bb279524095f8e48fa4e91c79d8
sardar umer <umer.javed@codistan.org>
2023-03-15 15:37:01 +0500

    resolved swiper issue and added add to cart

--- components/ProductDetail/ProductDetail.js
+++ components/ProductDetail/ProductDetail.js
@@ -2,7 +2,7 @@ import React, { Component, useEffect, Fragment } from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
-
+import withWidth, { isWidthUp, isWidthDown } from "@material-ui/core/withWidth";
 import inject from "hocs/inject";
 import Breadcrumbs from "components/Breadcrumbs";
 import ProductDetailAddToCart from "components/ProductDetailAddToCart";
@@ -15,9 +15,11 @@ import MediaGallery from "components/MediaGallery";
 import Router from "translations/i18nRouter";
 import priceByCurrencyCode from "lib/utils/priceByCurrencyCode";
 import variantById from "lib/utils/variantById";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Thumbs, Mousewheel, Pagination } from "swiper";
+const { Swiper, SwiperSlide } = require("swiper/react");
+const { Navigation, Thumbs, Mousewheel, Pagination } = require("swiper");
 import Box from "@material-ui/core/Box";
+
+import { makeStyles } from "@material-ui/core/styles";
 import { useRef, useCallback, useState } from "react";
 import Typography from "@material-ui/core/Typography";
 import Tab from "@material-ui/core/Tab";
@@ -224,7 +226,7 @@ const slide = [
 ];
 
 const ProductDetail = ({ ...props }) => {
-  console.log("dsddddd", ...props);
+  console.log(props.product,"new")
   const sliderRef = useRef(null);
 
   const handlePrev = useCallback(() => {
@@ -239,6 +241,7 @@ const ProductDetail = ({ ...props }) => {
   const [imagesNavSlider, setImagesNavSlider] = useState(null);
   const [value, setValue] = React.useState("1");
   const [activeIndex, setActiveIndex] = useState(0);
+    const [addToCartQuantity, setAddToCartQuantity] = useState(1);
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
@@ -398,197 +401,223 @@ const ProductDetail = ({ ...props }) => {
     }
   }
 
+   const handleOnClick = async () => {
+    
+
+    // Pass chosen quantity to onClick callback
+    await handleAddToCartClick(addToCartQuantity);
+
+    // Scroll to the top
+    if (typeof window !== "undefined" && typeof window.scrollTo === "function") {
+      window.scrollTo({ left: 0, top: 0, behavior: "smooth" });
+    }
+  }
   const productPrice = determineProductPrice();
   const compareAtDisplayPrice = (productPrice.compareAtPrice && productPrice.compareAtPrice.displayAmount) || null;
 
   console.log(pdpMediaItems);
   return (
     <>
-      {typeof window !== "undefined" && (
-        <>
-          <Box className={classes.slider}>
-            {/* <Grid container spacing={2} className={classes.sliderflex} xs={12} md={12} sm={12} lg={12} 
-
-
-  alignItems="center"
-  justifyContent="center"
-      >
-        <Grid item xs={0} md={2} sm={0} lg={2} className={classes.slidercol}>
-          <div className={classes.thumb}>
+      <Box className={classes.slider}>
+        <Grid
+          container
+          spacing={2}
+          className={classes.sliderflex}
+          xs={12}
+          md={12}
+          sm={12}
+          lg={12}
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Grid item xs={0} md={2} sm={0} lg={2} className={classes.slidercol}>
+            <div className={classes.thumb}>
+              <Swiper
+                onSwiper={setImagesNavSlider}
+                direction="vertical"
+                spaceBetween={24}
+                slidesPerView={3}
+                navigation={{
+                  nextEl: ".slider__next",
+                  prevEl: ".slider__prev",
+                }}
+                className={classes.container1}
+                breakpoints={{
+                  0: {
+                    direction: "horizontal",
+                  },
+                  768: {
+                    direction: "vertical",
+                  },
+                }}
+                modules={[Navigation, Thumbs]}
+              >
+                {slides.map((slide, index) => {
+                  return (
+                    <SwiperSlide key={index}>
+                      <div className={classes.thumbimage}>
+                        <img src={slide.image} alt="" className={classes.thumbimage} />
+                      </div>
+                    </SwiperSlide>
+                  );
+                })}
+              </Swiper>
+            </div>
+          </Grid>
+          <Grid item xs={12} md={10} sm={7} lg={5} className={`${classes.sliderimages} swiper_slider`}>
             <Swiper
-              onSwiper={setImagesNavSlider}
-              direction="vertical"
-              spaceBetween={24}
-              slidesPerView={3}
+              thumbs={{ swiper: imagesNavSlider }}
+              direction="horizontal"
+              slidesPerView={1}
+              spaceBetween={32}
+              ref={sliderRef}
+              pagination={{
+                clickable: true,
+              }}
+              mousewheel={true}
               navigation={{
                 nextEl: ".slider__next",
                 prevEl: ".slider__prev",
               }}
-              className={classes.container1}
               breakpoints={{
                 0: {
                   direction: "horizontal",
+                  thumbs: "false",
                 },
                 768: {
-                  direction: "vertical",
+                  direction: "horizontal",
                 },
               }}
-              modules={[Navigation, Thumbs]}
+              className={classes.container2}
+              modules={[Navigation, Thumbs, Mousewheel, Pagination]}
+              onRealIndexChange={(element) => setActiveIndex(element.activeIndex)}
             >
-              {slides.map((slide, index) => {
+              {slide.map((slide, index) => {
                 return (
                   <SwiperSlide key={index}>
-                    <div className={classes.thumbimage}>
-                      <img src={slide.image} alt="" className={classes.thumbimage} />
+                    <div className={classes.controller}>
+                      <img src={slide.image} alt="" className={classes.sliderimage2} />
+
+                      {activeIndex < slides.length - 1 ? (
+                        <ArrowForwardIos
+                          className={classes.iconforwad}
+                          style={{ fill: "#FDC114" }}
+                          onClick={handleNext}
+                        />
+                      ) : (
+                        ""
+                      )}
+                      {activeIndex - 0 ? (
+                        <ArrowBackIos className={classes.iconback} style={{ fill: "#FDC114" }} onClick={handlePrev} />
+                      ) : (
+                        ""
+                      )}
                     </div>
                   </SwiperSlide>
                 );
               })}
             </Swiper>
-          </div>
+          </Grid>
+
+          <Grid item xs={11} md={10} sm={6} lg={3}>
+            <div className={classes.carttext}>
+              <Typography variant="subtitle1">Floral Shirt in yellow color for sale on Bizb</Typography>
+              <div className={classes.size}>
+                {" "}
+                <div className={classes.size2}>
+                  {" "}
+                  <strike>
+                    {" "}
+                    <Typography gutterBottom variant="h4" className={classes.price2}>
+                      Rs 600
+                    </Typography>
+                  </strike>
+                  <Typography gutterBottom variant="h4" className={classes.price}>
+                    Rs 600
+                  </Typography>
+                </div>
+                <Typography gutterBottom variant="h4" className={classes.offer}>
+                  50 % OFF
+                </Typography>
+              </div>
+              <div className={classes.sizeimage}>
+                <img src="/cart/available.svg" alt="available" />
+                <Typography variant="h4" className={classes.offr}>
+                  Large
+                </Typography>
+              </div>
+              <div className={classes.cart}>
+                <img component="img" src="/icons/cart.svg" className={classes.cartimage} />
+                <Typography variant="h4" onClick={handleOnClick}>+ Cart </Typography>
+              </div>
+              <TabContext value={value}>
+                <TabList onChange={handleChange} className={classes.tabs}>
+                  <Tab label="Description" value="1" />
+                  <Tab label="size chart" value="2" />
+                </TabList>
+
+                <TabPanel value="1">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                  dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                  aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+                  officia deserunt mollit anim id est laborum.
+                </TabPanel>
+
+                <TabPanel value="2">
+                  ffffffffffffffffffff voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                  occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </TabPanel>
+              </TabContext>
+            </div>
+          </Grid>
         </Grid>
-        <Grid item xs={12} md={10} sm={7} lg={5} className={`${classes.sliderimages} swiper_slider`}>
-          <Swiper
-            thumbs={{ swiper: imagesNavSlider }}
-            direction="horizontal"
-            slidesPerView={1}
-            spaceBetween={32}
-            ref={sliderRef}
-            pagination={{
-              clickable: true,
-            }}
-            mousewheel={true}
-            navigation={{
-              nextEl: ".slider__next",
-              prevEl: ".slider__prev",
-            }}
-            breakpoints={{
-              0: {
-                direction: "horizontal",
-                thumbs: "false",
-              },
-              768: {
-                direction: "horizontal",
-              },
-            }}
-            className={classes.container2}
-            modules={[Navigation, Thumbs, Mousewheel, Pagination]}
-            onRealIndexChange={(element)=>setActiveIndex(element.activeIndex)}
-          >
-             
-            {slide.map((slide, index) => {
-              return (
-                <SwiperSlide key={index}>
-                   <div className={classes.controller}>
-                  <img src={slide.image} alt="" className={classes.sliderimage2} />
-                 
+      </Box>
 
-{  activeIndex < slides.length-1 ?   <ArrowForwardIos className={classes.iconforwad} style={{fill: "#FDC114"}} onClick={handleNext}/>:""}
-{activeIndex-0?<ArrowBackIos className={classes.iconback} style={{fill: "#FDC114"}}  onClick={handlePrev}/>:""}
-</div>
-                </SwiperSlide>
-              );
-            })}
-  
-          </Swiper>
+      <Fragment>
+        <Grid container spacing={5}>
+          <Grid item className={classes.breadcrumbGrid} xs={12}>
+            <Breadcrumbs isPDP tagId={routingStore.tagId} product={product} />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <div className={classes.section}>
+              <MediaGallery mediaItems={pdpMediaItems} />
+            </div>
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <ProductDetailTitle pageTitle={product.pageTitle} title={product.title} />
+            <div className={classes.info}>
+              <ProductDetailVendor>{product.vendor}</ProductDetailVendor>
+            </div>
+            <div className={classes.info}>
+              <ProductDetailPrice
+                className={classes.bottomMargin}
+                compareAtPrice={compareAtDisplayPrice}
+                price={productPrice.displayPrice}
+              />
+            </div>
+            <div className={classes.info}>
+              <ProductDetailDescription>{product.description}</ProductDetailDescription>
+            </div>
+            <VariantList
+              onSelectOption={handleSelectOption}
+              onSelectVariant={handleSelectVariant}
+              product={product}
+              selectedOptionId={pdpSelectedOptionId}
+              selectedVariantId={pdpSelectedVariantId}
+              currencyCode={currencyCode}
+              variants={product.variants}
+            />
+            <ProductDetailAddToCart
+              onClick={handleAddToCartClick}
+              selectedOptionId={pdpSelectedOptionId}
+              selectedVariantId={pdpSelectedVariantId}
+              variants={product.variants}
+            />
+          </Grid>
         </Grid>
-        
-        <Grid item xs={11} md={10} sm={6} lg={3}  >
-          <div className={classes.carttext}>
-          <Typography variant="subtitle1">Floral Shirt in yellow color for sale on Bizb</Typography>
-          <div className={classes.size}>
-            {" "}
-           
-           <div className={classes.size2}> <strike > <Typography gutterBottom variant="h4" className={classes.price2}>
-              Rs 600
-            </Typography></strike>
-            <Typography gutterBottom variant="h4" className={classes.price}>
-              Rs 600
-            </Typography></div>
-            <Typography gutterBottom variant="h4" className={classes.offer}>
-              50 % OFF
-            </Typography>
-          </div>
-          <div className={classes.sizeimage}>
-            <img src="/cart/available.svg" alt="available" />
-            <Typography  variant="h4" className={classes.offr}>
-              Large
-            </Typography>
-          </div>
-          <div className={classes.cart}>
-            <img component="img" src="/icons/cart.svg" className={classes.cartimage} />
-            <Typography  variant="h4">
-              + Cart{" "}
-            </Typography>
-          </div>
-          <TabContext value={value}>
-            <TabList onChange={handleChange} className={classes.tabs}>
-              <Tab label="Description" value="1" />
-              <Tab label="size chart" value="2" />
-            </TabList>
-
-            <TabPanel value="1">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-
-Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </TabPanel>
-
-            <TabPanel value="2">
-              ffffffffffffffffffff voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-              cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </TabPanel>
-          </TabContext>
-          </div>
-        </Grid>
-      </Grid> */}
-          </Box>
-
-          <Fragment>
-            <Grid container spacing={5}>
-              <Grid item className={classes.breadcrumbGrid} xs={12}>
-                <Breadcrumbs isPDP tagId={routingStore.tagId} product={product} />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <div className={classes.section}>
-                  <MediaGallery mediaItems={pdpMediaItems} />
-                </div>
-              </Grid>
-
-              <Grid item xs={12} sm={6}>
-                <ProductDetailTitle pageTitle={product.pageTitle} title={product.title} />
-                <div className={classes.info}>
-                  <ProductDetailVendor>{product.vendor}</ProductDetailVendor>
-                </div>
-                <div className={classes.info}>
-                  <ProductDetailPrice
-                    className={classes.bottomMargin}
-                    compareAtPrice={compareAtDisplayPrice}
-                    price={productPrice.displayPrice}
-                  />
-                </div>
-                <div className={classes.info}>
-                  <ProductDetailDescription>{product.description}</ProductDetailDescription>
-                </div>
-                <VariantList
-                  onSelectOption={handleSelectOption}
-                  onSelectVariant={handleSelectVariant}
-                  product={product}
-                  selectedOptionId={pdpSelectedOptionId}
-                  selectedVariantId={pdpSelectedVariantId}
-                  currencyCode={currencyCode}
-                  variants={product.variants}
-                />
-                <ProductDetailAddToCart
-                  onClick={handleAddToCartClick}
-                  selectedOptionId={pdpSelectedOptionId}
-                  selectedVariantId={pdpSelectedVariantId}
-                  variants={product.variants}
-                />
-              </Grid>
-            </Grid>
-          </Fragment>
-        </>
-      )}
+      </Fragment>
     </>
   );
 };
@@ -611,4 +640,6 @@ ProductDetail.propTypes = {
   uiStore: PropTypes.object.isRequired,
   width: PropTypes.string.isRequired,
 };
-export default withStyles(styles, { withTheme: true })(inject("routingStore", "uiStore")(ProductDetail));
+export default withWidth({ initialWidth: "md" })(
+  withStyles(styles, { withTheme: true })(inject("routingStore", "uiStore")(ProductDetail)),
+);
