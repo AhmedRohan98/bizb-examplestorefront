814b5db94a6ee6d83c392b05d6a9bb6d4fb95fba
Bilal Isa Khan <bilalisakhan97@gmail.com>
2023-04-06 10:29:49 +0500

    add: fix

--- components/Preloved/prelovedSec.js
+++ components/Preloved/prelovedSec.js
@@ -107,7 +107,6 @@ const useStyles = makeStyles((theme) => ({
     background: theme.palette.secondary.selected,
     display: "flex",
     justifyContent: "center",
-    display: "flex",
     alignItems: "center",
     width: "150px",
     height: "35px",
