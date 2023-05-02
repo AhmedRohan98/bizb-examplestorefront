fa269860cbb12210d5d9c86bf175412d5c0fdc5e
sardar umer <umer.javed@codistan.org>
2023-05-03 01:31:48 +0500

    footer ui setting

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -5,154 +5,22 @@ import { styled } from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
+
 import ProgressiveImage from "components/ProgressiveImage";
 import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
 const date = new Date();
 
 
 const useStyles = makeStyles((theme) => ({
-  main: {
-    width: "100%",
-    padding: "75px",
-    [theme.breakpoints.down("xs")]: {
-      padding: "0",
-    },
-  },
-  cardaction: {
-    height: 312,
-    width: 312,
-  },
-  root: {
-    display: "flex",
-    width: "100%",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-  gridroot: {
-    width: "100%",
-    display: "flex",
-    alignItems: "baseline",
-    position: "relative",
-    justifyContent: "space-between",
-  },
-  typography: {
-    background: "#333333",
-    opacity: "15%",
-    height: "8px",
-    width: "180px",
-  },
-  text: {
-    position: "absolute",
-    bottom: 60,
-  },
-  header: {
-    height: "50px",
-    position: "relative",
-  },
-  headermain: {
-    display: "flex",
-    justifyContent: "space-between",
-  },
-  image: {
-    width: "312px",
-    maxHeight: "450px",
-    objectFit: "cover",
-    borderRadius: "10px",
-    cursor: "pointer",
-  },
-  size: {
-    display: "flex",
-    flexDirection: "row",
-    marginLeft: theme.spacing(1),
-  },
-  cartimage: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "flex-start",
-  },
-  carttitle: {
-    display: "flex",
-    marginLeft: theme.spacing(1),
-    justifyContent: "flex-start",
-    alignItems: "flex-start",
-  },
-  price: {
-    marginLeft: "20px",
-    fontWeight: "700",
-    fontSize: "20px",
-  },
-  rootimg: {
-    position: "relative",
-    display: "inline-grid",
-    width: "312px",
-
-    maxWidth: "312px",
-    marginLeft: "10px",
-    marginRight: "10px",
-  },
-  cartbackground: {
-    background: "linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.6) 100%)",
-    borderRadius: "0px 0px 16px 16px",
-    alignItems: "center",
-    justifyContent: "initial",
-    height: "75px",
-    width: "100%",
-    bottom: "20%",
-    display: "inline-grid",
-    width: "100%",
-    marginTop: " -75px",
-    padding: "13px 20px",
-  },
-  cart: {
-    height: "35px",
-    width: "84px",
-    borderRadius: "40px",
-    background: "#FDC114",
-    cursor: "pointer",
-    display: "flex",
-    justifyContent: "space-evenly",
-    alignItems: "center",
-    borderColor: "none",
-    zIndex: 1200,
-    transition: "all 0.2s linear",
-    "&:hover": {
-      transform: "scale(1.08)",
-      transition: "left 0.2s linear",
-      background: "#FDC114",
-    },
-  },
-  explore: {
-    position: "absolute",
-    top: "6px",
-    right: "10px",
-    color: "#FDC114",
-    zIndex: 900,
-  },
-  maintitle: {
-    display: "flex",
-    justifyContent: "flex-start",
-    alignItems: "flex-start",
-    width: "312px",
-    flexDirection: "column",
-  },
-  spanofnextword: {
-    color: "#FDC114",
-  },
-  toast: {
-    background: "yellow",
-    color: "black",
-  },
-  pricing: {
-    display: "flex",
-    flexDirection: "row",
-    marginLeft: theme.spacing(1),
-    marginBottom: theme.spacing(2),
-  },
+ footerthirdsec:{
+  textAlign:"center"
+ }
 }));
 
 
 const Footer = () =>{
+    const classes = useStyles();
   return (
     <footer style={{ backgroundColor: "#000000", color: "#FFFFFF" }}>
       <Grid container>
@@ -196,8 +64,11 @@ const Footer = () =>{
           </div>
         </Grid>
         <Grid item xs={12} md={5}>
-          <div style={{ fontWeight: "500", fontSize: "15px", textAlign: "center" }}>
-            <div style={{ marginTop: "80px" }}>FEEL FREE TO CONTACT US, ANYTIME, ANYWHERE</div>
+          <div className={classes.footerthirdsec}>
+            <div style={{ marginTop: "80px" }}></div>
+            <Typography variant="typography" style={{ fontWeight: 500 }}>
+              FEEL FREE TO CONTACT US, ANYTIME, ANYWHERE
+            </Typography>
             <div style={{ marginTop: "20px" }}>
               <span>
                 <img src="/images/emailIcon.svg" />
@@ -215,6 +86,7 @@ const Footer = () =>{
                 marginLeft: "10vh",
                 fontWeight: "500",
                 fontSize: "18px",
+               
                 textAlign: "center",
               }}
             >
