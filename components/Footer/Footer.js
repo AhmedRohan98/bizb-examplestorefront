2ab5b8186877bca3098234c9972fcaac19fa0bba
sardar umer <umer.javed@codistan.org>
2023-06-01 17:48:29 +0500

    footer icons with hover effect

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -40,6 +40,15 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: "center",
     alignItems: "center",
   },
+  imagesicons: {
+    margin: "2px",
+    cursor: "pointer",
+    "&:hover": {
+      transform: "scale(1.08)",
+      transition: "left 0.2s linear",
+     
+    },
+  },
 }));
 
 const Footer = () => {
@@ -56,10 +65,11 @@ const Footer = () => {
             </span>
           </div>
           <div style={{ marginTop: "10px", marginLeft: "90px" }}>
-            <img src="/icons/instagram.svg" />
-            <img src="/icons/facebook.svg" />
-            <img src="/icons/twitter.svg" />
-            <img src="/icons/linkedin.svg" />
+            <img src="/icons/instagram.svg" className={classes.imagesicons} />
+            <img src="/icons/facebook.svg" className={classes.imagesicons} />
+            <img src="/icons/twitter.svg" className={classes.imagesicons} />
+            <img src="/icons/linkedin2.svg" className={classes.imagesicons} />
+            <img src="/icons/youtube.svg" className={classes.imagesicons} />
           </div>
         </Grid>
         <Grid item xs={12} md={3}>
