6effd1d067d566bdb15a1b20dd7c218634a4d071
sobismhs <sobia.zulfiqar@codistan.org>
2023-08-15 15:28:53 +0500

    store prepage layout: box size fixed

--- hooks/sellers/useGetAllSeller.js
+++ hooks/sellers/useGetAllSeller.js
@@ -1,21 +1,21 @@
 import { useEffect } from "react";
 import { useQuery } from "@apollo/client";
 import getAllSeller from "./getAllSeller.gql";
-
 /**
  * Gets current viewer's data
  *
  * @returns {Array} the viewer's data
  */
-export default function useGetAllSeller () {
+export default function useGetAllSeller(query) {
   const authToken = typeof window !== "undefined" ? window.localStorage.getItem("accounts:accessToken") : undefined;
-
-  const { loading, data, refetch } =useQuery(getAllSeller);
-
+  const { loading, data, refetch } = useQuery(getAllSeller, {
+    variables: {
+      query,
+    },
+  });
   const sellers = data?.getAllSeller;
   useEffect(() => {
     refetch();
   }, [authToken]);
-
   return [sellers, loading, refetch];
 }
